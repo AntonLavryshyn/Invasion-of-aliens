47c62df4f4ed87367f413c63f91e90c765bee3fc
Anton <antonlavryshyn@gmail.com>
2023-03-15 17:50:22 +0200

    Fifth commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,6 +25,7 @@ class AlienInvasion:
 
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             """Monitor mouse and keyboard events"""
 
@@ -35,6 +36,22 @@ class AlienInvasion:
 
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    """move the ship to the right"""
+                    self.ship.moving_right = True
+
+                elif event.key == pygame.K_LEFT:
+                    """move the ship to the left"""
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update image on screen and switch to a new screen"""
--- ship.py
+++ ship.py
@@ -12,11 +12,24 @@ class Ship:
 
         """Download ship image and get it in "react()" """
 
-        self.image = pygame.image.load('images/space_ship.bmp')
+        self.image = pygame.image.load('images/space_ship_small.bmp')
         self.rect = self.image.get_rect()
 
         """Create a new each ship at the bottom of the screen, in the center"""
         self.rect.midbottom = self.screen_rect.midbottom
+
+        """traffic indicator"""
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update the ship's current position based on the motion indicator"""
+
+        if self.moving_right:
+            self.rect.x += 1
+
+        if self.moving_left:
+            self.rect.x -= 1
 
     def draw_a_ship(self):
         """draw the ship in its current location"""
